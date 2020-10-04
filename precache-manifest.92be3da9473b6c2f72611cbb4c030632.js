9635df267e83422817f98a3e78abb0931dd5c593
kzk <qwertykzk@gmail.com>
2020-10-04 17:55:40 +0530

    Updates

---
+++ precache-manifest.92be3da9473b6c2f72611cbb4c030632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7d28e4b03cc4fc8e14a30a19643314",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "4b10fd50a2860f3deb80",
+    "url": "/todoList/static/css/main.4c76717b.chunk.css"
+  },
+  {
+    "revision": "2958eca730c1c46f65f4",
+    "url": "/todoList/static/js/2.221a327e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoList/static/js/2.221a327e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b10fd50a2860f3deb80",
+    "url": "/todoList/static/js/main.816099eb.chunk.js"
+  },
+  {
+    "revision": "085b75269da73d04bf5a",
+    "url": "/todoList/static/js/runtime-main.a02a05ee.js"
+  }
+]);
